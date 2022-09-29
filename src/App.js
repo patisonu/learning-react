1e30d698ec3f16267e641a0173dffff1fe0b704b
patisonu <sonu@elicit.digital>
2022-09-29 12:46:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './component/Navbar';
+import Formtext from './component/Formtext';
+
+function App() {
+  return (
+   <>
+   <Navbar title="Sonu"></Navbar>
+   <div className="container">
+     <Formtext></Formtext>
+   </div>
+   </>
+  );
+}
+
+export default App;
---
+++ src/component/Formtext.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export default function Formtext() {
+   
+    const handleUpClick = ()=>{
+        let newtext = areaval.toUpperCase();
+        setAreaval(newtext);
+    }
+    const handleOnChange = (event)=>{
+        setAreaval(event.target.value);
+    }
+    const [areaval, setAreaval] = useState('Enter text here');
+    return (
+    <>
+     <h2>Write Something</h2>
+        <div className="mb-3">
+            <textarea className="form-control" value={areaval} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+        </div><button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+        <div className="my-2">
+            <p>Words {areaval.split(" ").length}</p>
+            <p>Characters {areaval.length}</p>
+            <p>Words</p>
+        </div>
+    </>
+    )
+}
